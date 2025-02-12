4efb98c8735d81a36bab2d20e97907d534878a69
elderkingsley <elderkingsley@gmail.com>
2025-02-12 16:31:07 +0100

    setting up live wire and admin panel

---
+++ bootstrap/cache/filament/panels/admin.php
@@ -0,0 +1,61 @@
+<?php return array (
+  'livewireComponents' => 
+  array (
+    'filament.pages.dashboard' => 'Filament\\Pages\\Dashboard',
+    'filament.widgets.account-widget' => 'Filament\\Widgets\\AccountWidget',
+    'filament.widgets.filament-info-widget' => 'Filament\\Widgets\\FilamentInfoWidget',
+    'filament.livewire.database-notifications' => 'Filament\\Livewire\\DatabaseNotifications',
+    'filament.pages.auth.edit-profile' => 'Filament\\Pages\\Auth\\EditProfile',
+    'filament.livewire.global-search' => 'Filament\\Livewire\\GlobalSearch',
+    'filament.livewire.notifications' => 'Filament\\Livewire\\Notifications',
+    'filament.pages.auth.login' => 'Filament\\Pages\\Auth\\Login',
+  ),
+  'clusters' => 
+  array (
+  ),
+  'clusteredComponents' => 
+  array (
+  ),
+  'clusterDirectories' => 
+  array (
+  ),
+  'clusterNamespaces' => 
+  array (
+  ),
+  'pages' => 
+  array (
+    0 => 'Filament\\Pages\\Dashboard',
+  ),
+  'pageDirectories' => 
+  array (
+    0 => '/home/elder/gitlab/blackfire/factory/app/Filament/Pages',
+  ),
+  'pageNamespaces' => 
+  array (
+    0 => 'App\\Filament\\Pages',
+  ),
+  'resources' => 
+  array (
+  ),
+  'resourceDirectories' => 
+  array (
+    0 => '/home/elder/gitlab/blackfire/factory/app/Filament/Resources',
+  ),
+  'resourceNamespaces' => 
+  array (
+    0 => 'App\\Filament\\Resources',
+  ),
+  'widgets' => 
+  array (
+    0 => 'Filament\\Widgets\\AccountWidget',
+    1 => 'Filament\\Widgets\\FilamentInfoWidget',
+  ),
+  'widgetDirectories' => 
+  array (
+    0 => '/home/elder/gitlab/blackfire/factory/app/Filament/Widgets',
+  ),
+  'widgetNamespaces' => 
+  array (
+    0 => 'App\\Filament\\Widgets',
+  ),
+);
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'elder',
+            'email' => 'kingsley@blackfirebarbecue.com',
+            'password' => bcrypt('Jedikoko419#'),
         ]);
     }
 }
